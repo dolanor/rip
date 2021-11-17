ced30cb92df9f5ef1b51e953df3656d8944690d9
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2021-11-17 19:03:58 +0100

    add encoder selection

---
+++ encoding.go
@@ -0,0 +1,37 @@
+package rip
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io"
+)
+
+type Decoder interface {
+	Decode(v interface{}) error
+}
+
+func ContentTypeDecoder(r io.Reader, contentTypeHeader string) Decoder {
+	switch contentTypeHeader {
+	case "text/xml":
+		return xml.NewDecoder(r)
+	case "text/json":
+		fallthrough
+	default:
+		return json.NewDecoder(r)
+	}
+}
+
+type Encoder interface {
+	Encode(v interface{}) error
+}
+
+func AcceptEncoder(w io.Writer, acceptHeader string) Encoder {
+	switch acceptHeader {
+	case "text/xml":
+		return xml.NewEncoder(w)
+	case "text/json":
+		fallthrough
+	default:
+		return json.NewEncoder(w)
+	}
+}
