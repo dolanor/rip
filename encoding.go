b853d1865b387b3dc2907f12cb27f9137daa80be
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2023-05-31 14:03:15 +0200

    improve HTMLEncoder

--- encoding.go
+++ encoding.go
@@ -10,18 +10,23 @@ import (
 	"net/http"
 	"reflect"
 	"strings"
+	"time"
+
+	"github.com/ajg/form"
 )
 
 var AvailableEncodings = []string{
 	"application/json",
 	"text/xml",
 	"text/html",
+	"application/x-www-form-urlencoded",
 }
 
 var AvailableCodecs = map[string]Codec{
-	"text/xml":  {NewEncoder: WrapEncoder(xml.NewEncoder), NewDecoder: WrapDecoder(xml.NewDecoder)},
-	"text/html": {NewEncoder: WrapEncoder(NewHTMLEncoder), NewDecoder: WrapDecoder(NewHTMLDecoder)},
 	"application/json":                  {NewEncoder: WrapEncoder(json.NewEncoder), NewDecoder: WrapDecoder(json.NewDecoder)},
+	"text/xml":                          {NewEncoder: WrapEncoder(xml.NewEncoder), NewDecoder: WrapDecoder(xml.NewDecoder)},
+	"text/html":                         {NewEncoder: WrapEncoder(NewHTMLEncoder), NewDecoder: WrapDecoder(NewHTMLDecoder)},
+	"application/x-www-form-urlencoded": {NewEncoder: WrapEncoder(NewHTMLFormEncoder), NewDecoder: WrapDecoder(form.NewDecoder)},
 }
 
 type HTMLFormEncoder struct {
@@ -51,37 +56,25 @@ func HTMLEncode(w io.Writer, edit EditMode, v interface{}) error {
 	if s.Kind() == reflect.Pointer {
 		s = s.Elem()
 	}
-	t := s.Type()
-	name := t.Name()
-	//log.Println("encode: name:", name)
+
+	var resources []resource
+	if s.Kind() == reflect.Slice {
+		for i := 0; i < s.Len(); i++ {
+			s := s.Index(i)
+
+			res := expandFields(s)
+
+			resources = append(resources, res)
+		}
+	} else {
+		res := expandFields(s)
+		resources = append(resources, res)
+	}
 
 	rw, ok := w.(http.ResponseWriter)
 	if ok {
 		// We force Content-Type to HTML
 		rw.Header().Set("Content-Type", "text/html")
-	}
-
-	type field struct {
-		Key   string
-		Value any
-	}
-
-	type resource struct {
-		ID     string
-		Name   string
-		Fields []field
-	}
-
-	res := resource{
-		Name: name,
-	}
-	for i := 0; i < s.NumField(); i++ {
-		f := s.Field(i)
-		fName := t.Field(i).Name
-		//fType := f.Type()
-		fVal := f.Interface()
-		//log.Printf("fieldX : %s: %+v || %+v", fName, f.Type(), f.Interface())
-		res.Fields = append(res.Fields, field{fName, fVal})
 	}
 
 	tpl := template.New("resource").Funcs(template.FuncMap{
@@ -102,12 +95,53 @@ func HTMLEncode(w io.Writer, edit EditMode, v interface{}) error {
 		return err
 	}
 
-	err = tpl.Execute(w, res)
+	err = tpl.Execute(w, resources)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+type field struct {
+	Key   string
+	Value any
+}
+
+type resource struct {
+	ID     any
+	Name   string
+	Fields []field
+}
+
+func expandFields(s reflect.Value) resource {
+	t := s.Type()
+	name := t.Name()
+	// log.Println("encode: name:", name)
+	if s.Kind() == reflect.Pointer {
+		s = s.Elem()
+		t = s.Type()
+	}
+
+	res := resource{
+		Name: name,
+	}
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		fName := t.Field(i).Name
+		// fType := f.Type()
+		fVal := f.Interface()
+		if f.Type() == reflect.TypeOf(time.Time{}) {
+			fVal = f.Interface().(time.Time).Format(time.RFC3339)
+		}
+		// log.Printf("fieldX : %s: %+v || %+v", fName, f.Type(), f.Interface())
+		if fName == "ID" {
+			res.ID = fVal
+		}
+		res.Fields = append(res.Fields, field{fName, fVal})
+	}
+
+	return res
 }
 
 //go:embed resource.gotpl
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/dolanor/rip
 
 go 1.18
 
-require github.com/gorilla/handlers v1.5.1
+require (
+	github.com/ajg/form v1.5.1
+	github.com/gorilla/handlers v1.5.1
+)
 
 require github.com/felixge/httpsnoop v1.0.1 // indirect
