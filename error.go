d31feb0b2e7fc0c6488a70438a0c83305eeb8b99
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2024-03-13 03:48:56 +0100

    rename Link to ErrorLink
    it is only used as web link in the context of errors

--- error.go
+++ error.go
@@ -50,7 +50,7 @@ type Error struct {
 	ID string `json:"id,omitempty"`
 
 	// Links can contains an About Link or a Type Link.
-	Links []Link `json:"links,omitempty"`
+	Links []ErrorLink `json:"links,omitempty"`
 
 	// Status is the HTTP status code applicable to this problem. This SHOULD be provided.
 	Status int `json:"status,omitempty"`
@@ -155,4 +155,25 @@ func (e badRequestError) Unwrap() error {
 		return err
 	}
 	return nil
+}
+
+// ErrorLink represents a RFC8288 web link.
+type ErrorLink struct {
+	// HRef is a URI-reference [RFC3986 Section 4.1] pointing to the link’s target.
+	HRef string `json:"href,omitempty"`
+
+	// Rel indicates the link’s relation type. The string MUST be a valid link relation type.
+	Rel string `json:"rel,omitempty"`
+
+	// DescribedBy is a link to a description document (e.g. OpenAPI or JSON Schema) for the link target.
+	DescribedBy *ErrorLink `json:"describedby,omitempty"`
+
+	// Title serves as a label for the destination of a link such that it can be used as a human-readable identifier (e.g., a menu entry).
+	Title string `json:"title,omitempty"`
+
+	// Type indicates the media type of the link’s target.
+	Type string `json:"type,omitempty"`
+
+	// HRefLang indicates the language(s) of the link’s target. An array of strings indicates that the link’s target is available in multiple languages. Each string MUST be a valid language tag [RFC5646].
+	HRefLang []string `json:"hreflang,omitempty"`
 }
