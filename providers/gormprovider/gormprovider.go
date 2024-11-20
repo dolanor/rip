c9a4e23b269c88db849531868233c06d40a498f1
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2024-11-20 11:48:32 +0100

    improve gormprovider logging
    - all: log entity type
    - POST: log ID when it is set, not before (by using defer)
    - GET list: log offset, limit and entities list size

--- providers/gormprovider/gormprovider.go
+++ providers/gormprovider/gormprovider.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"log/slog"
+	"reflect"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -36,7 +37,7 @@ type gormEntityProvider[Ent any] struct {
 
 func (ep *gormEntityProvider[Ent]) Create(ctx context.Context, e Ent) (Ent, error) {
 	id, err := ripreflect.GetID(e)
-	ep.logger.Info("create", "id", id)
+	defer func() { ep.logger.Info("create", "entity", reflect.TypeOf(e).Name(), "id", id) }()
 	if err != nil {
 		return e, err
 	}
@@ -46,6 +47,7 @@ func (ep *gormEntityProvider[Ent]) Create(ctx context.Context, e Ent) (Ent, erro
 		if err != nil {
 			return e, errors.New("can not generate unique id")
 		}
+		id = uuid.String()
 
 		err = ripreflect.SetID(&e, uuid.String())
 		if err != nil {
@@ -63,9 +65,8 @@ func (ep *gormEntityProvider[Ent]) Create(ctx context.Context, e Ent) (Ent, erro
 }
 
 func (ep *gormEntityProvider[Ent]) Delete(ctx context.Context, id string) error {
-	ep.logger.Info("delete", "id", id)
-
 	var e Ent
+	ep.logger.Info("delete", "entity", reflect.TypeOf(e).Name(), "id", id)
 	err := ripreflect.SetID(&e, id)
 	if err != nil {
 		return err
@@ -81,7 +82,7 @@ func (ep *gormEntityProvider[Ent]) Delete(ctx context.Context, id string) error 
 
 func (ep *gormEntityProvider[Ent]) Update(ctx context.Context, e Ent) error {
 	id, err := ripreflect.GetID(e)
-	ep.logger.Info("update", "id", id)
+	ep.logger.Info("update", "entity", reflect.TypeOf(e).Name(), "id", id)
 	if err != nil {
 		return err
 	}
@@ -99,7 +100,7 @@ func (ep *gormEntityProvider[Ent]) Get(ctx context.Context, id string) (Ent, err
 		return e, nil
 	}
 
-	ep.logger.Info("get", "id", id)
+	ep.logger.Info("get", "entity", reflect.TypeOf(e).Name(), "id", id)
 
 	idFieldName := ripreflect.FieldIDName(e)
 	if idFieldName == ripreflect.MissingIDField {
@@ -115,9 +116,11 @@ func (ep *gormEntityProvider[Ent]) Get(ctx context.Context, id string) (Ent, err
 }
 
 func (ep *gormEntityProvider[Ent]) List(ctx context.Context, offset, limit int) ([]Ent, error) {
-	ep.logger.Info("list")
-
+	var e Ent
 	var ee []Ent
+	defer func() {
+		ep.logger.Info("list", "entity", reflect.TypeOf(e).Name(), "offset", offset, "limit", limit, "size", len(ee))
+	}()
 
 	tx := ep.db.
 		Offset(offset).
