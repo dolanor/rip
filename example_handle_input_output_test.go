2628f2498c8697b69e43d116f233104bbbb5c778
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2025-05-31 02:06:46 +0200

    doc: add code example for rip.Handle

---
+++ example_handle_input_output_test.go
@@ -0,0 +1,59 @@
+package rip_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	"github.com/dolanor/rip"
+	"github.com/dolanor/rip/encoding/json"
+)
+
+func ExampleHandle() {
+	toUpper := func(ctx context.Context, input string) (output string, err error) {
+		output = strings.ToUpper(input)
+		return output, nil
+	}
+
+	handler := rip.Handle(http.MethodPost, toUpper, rip.NewRouteOptions().WithCodecs(json.Codec))
+
+	http.HandleFunc("/uppercase/", handler)
+
+	slog.Info("listening on :8888")
+	http.ListenAndServe(":8888", nil)
+}
+
+func toUpper(ctx context.Context, input string) (output string, err error) {
+	output = strings.ToUpper(input)
+	return output, nil
+}
+
+func ExampleHandle_v2() {
+	handler := rip.Handle(http.MethodPost, toUpper, rip.NewRouteOptions().WithCodecs(json.Codec))
+	http.HandleFunc("/uppercase/", handler)
+
+	ts := httptest.NewServer(http.DefaultServeMux)
+	defer ts.Close()
+
+	buf := bytes.NewBufferString(`"hello world"`)
+
+	res, err := http.Post(ts.URL+"/uppercase/", "application/json", buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	greeting, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", greeting)
+	// Output: "HELLO WORLD"
+}
